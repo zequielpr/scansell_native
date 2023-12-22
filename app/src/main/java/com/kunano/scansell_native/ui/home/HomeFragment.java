1d45bbc272d6aac5d8078677ecd4c8e709d8cd52
zequielpr <zequielperez.r@gmail.com>
2023-12-22 02:30:25 +0000

    Button to create businesses has been placed on the app bar of the home screen

--- app/src/main/java/com/kunano/scansell_native/ui/home/HomeFragment.java
+++ app/src/main/java/com/kunano/scansell_native/ui/home/HomeFragment.java
@@ -4,17 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.kunano.scansell_native.R;
 import com.kunano.scansell_native.controllers.PruebaController;
 import com.kunano.scansell_native.controllers.negocio.NegociosController;
 import com.kunano.scansell_native.databinding.FragmentHomeBinding;
+import com.kunano.scansell_native.databinding.ToolBarHomeBinding;
 import com.kunano.scansell_native.model.negocio.Negocios;
-import com.kunano.scansell_native.model.negocio.Prueba;
 
 public class HomeFragment extends Fragment {
     private FragmentHomeBinding binding;
@@ -35,6 +38,12 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        ToolBarHomeBinding toolBarHomeBinding = binding.includeToolbar;
+        Toolbar toolbarHoma = toolBarHomeBinding.toolbar;
+        TextView title = toolbarHoma.findViewById(R.id.prueba);
+        title.setText(getString(R.string.businesses_title));
+       // toolBarHomeBinding.toolbar.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+
         RecyclerView businessLIst = binding.businessList;
 
 
@@ -52,11 +61,7 @@ public class HomeFragment extends Fragment {
 
 
 
-    public Prueba getPruebaFromDataBase(){
-        Prueba prueba = new Prueba();
-        prueba.setNombre("zequiel");
-        return  prueba;
-    }
+  public void   showInterfTocreatBusiness(){}
 
     @Override
     public void onDestroyView() {
