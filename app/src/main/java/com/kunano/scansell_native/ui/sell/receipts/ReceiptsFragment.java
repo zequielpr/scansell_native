581dbea923974b9961c084e16d988a0d261c7fb1
zequielpr <zequielperez.r@gmail.com>
2024-04-16 10:23:07 +0100

    spotted bugs on the sell section have been fixed

--- app/src/main/java/com/kunano/scansell_native/ui/sell/receipts/ReceiptsFragment.java
+++ app/src/main/java/com/kunano/scansell_native/ui/sell/receipts/ReceiptsFragment.java
@@ -47,12 +47,17 @@ public class ReceiptsFragment extends Fragment implements MenuProvider {
     private Toolbar toolbar;
     private SharedPreferences sharedPref;
     private SharedPreferences.Editor editor;
-    private static String DELETE_AFTER_15_DAYS = "DELETE_AFTER_15_DAYS";
+    private static final String DELETE_AFTER_15_DAYS = "DELETE_AFTER_15_DAYS";
+    private static final String DELETE_AFTER_30_DAYS = "DELETE_AFTER_30_DAYS";
+    private static final String NEVER = "NEVER";
+    private static final String DELETE_MODE = "DELETE_MODE";
     private SearchView searchView;
 
     private  ProcessItemsComponent<Receipt> processItemsComponent;
     private Drawable checkedCircle;
     private Drawable unCheckedCircle;
+
+    private View receiptEmptySectionLayout;
 
 
     public static ReceiptsFragment newInstance() {
@@ -82,6 +87,7 @@ public class ReceiptsFragment extends Fragment implements MenuProvider {
         unCheckedCircle = ContextCompat.getDrawable(getContext(), R.drawable.unchked_circle);
 
         recyclerViewReceipt = binding.receiptRecycleView;
+        receiptEmptySectionLayout = binding.notReceiptLayout.receiptEmptySection;
         recyclerViewReceipt.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerViewReceipt.setHasFixedSize(true);
         receiptAdapter = new ReceiptAdapter();
@@ -289,6 +295,8 @@ public class ReceiptsFragment extends Fragment implements MenuProvider {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         toolbar.addMenuProvider(this);
+        sellViewModel.getEmptyReceiptSectionVisibility().observe(getViewLifecycleOwner(),
+                receiptEmptySectionLayout::setVisibility);
     }
 
 
@@ -325,12 +333,19 @@ public class ReceiptsFragment extends Fragment implements MenuProvider {
         sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
         editor = sharedPref.edit();
 
-        boolean isDeleteAfter15Active = sharedPref.getBoolean(DELETE_AFTER_15_DAYS, true);
+        String deleteMode = sharedPref.getString(DELETE_MODE, NEVER);
 
-        if (isDeleteAfter15Active){
-            toolbar.getMenu().findItem(R.id.delete_15_days).setChecked(true);
-        }else {
-            toolbar.getMenu().findItem(R.id.delete_30_days).setChecked(true);
+
+        switch (deleteMode){
+            case DELETE_AFTER_15_DAYS:
+                toolbar.getMenu().findItem(R.id.delete_15_days).setChecked(true);
+                break;
+            case DELETE_AFTER_30_DAYS:
+                toolbar.getMenu().findItem(R.id.delete_30_days).setChecked(true);
+                break;
+            default:
+                toolbar.getMenu().findItem(R.id.never_delete).setChecked(true);
+
         }
     }
 
@@ -339,11 +354,15 @@ public class ReceiptsFragment extends Fragment implements MenuProvider {
 
         switch (menuItem.getItemId()){
             case R.id.delete_15_days:
-                deletePeriod15Days(true);
+                setDeleteMode(DELETE_AFTER_15_DAYS);
                 menuItem.setChecked(true);
                 return true;
             case R.id.delete_30_days:
-                deletePeriod15Days(false);
+                setDeleteMode(DELETE_AFTER_30_DAYS);
+                menuItem.setChecked(true);
+                return true;
+            case R.id.never_delete:
+                setDeleteMode(NEVER);
                 menuItem.setChecked(true);
                 return true;
             case R.id.delete_button:
@@ -371,9 +390,9 @@ public class ReceiptsFragment extends Fragment implements MenuProvider {
     }
 
 
-    private void deletePeriod15Days(Boolean delete_after_15_days){
+    private void setDeleteMode(String deleteMode){
         if(editor != null){
-            editor.putBoolean(DELETE_AFTER_15_DAYS,delete_after_15_days);
+            editor.putString(DELETE_MODE, deleteMode);
             editor.apply();
         }
     }
