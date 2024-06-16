e6a3d11cd3365cff00ef298eda1478e73a5b8e8e
zequielpr <zequielperez.r@gmail.com>
2024-06-16 04:52:52 +0100

    Last changes

--- app/src/main/java/com/kunano/scansell/ui/introduction/IntroductionActivity.java
+++ app/src/main/java/com/kunano/scansell/ui/introduction/IntroductionActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import com.kunano.scansell.databinding.IntroductionActivityBinding;
 
 
--- app/src/main/java/com/kunano/scansell/ui/introduction/sections/SectionOneFragment.java
+++ app/src/main/java/com/kunano/scansell/ui/introduction/sections/SectionOneFragment.java
@@ -36,6 +36,7 @@ public class SectionOneFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         binding = FragmentSectionOneBinding.inflate(getLayoutInflater());
 
